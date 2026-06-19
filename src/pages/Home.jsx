ff78e594801c5bcc06258303fc916c62051b5edd
Siddhant Pitale <166297685+KALKI16108@users.noreply.github.com>
2026-06-19 08:51:59 +0530

    Restore deleted Navbar and Home components from commit 47ca6f9

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import WhyAiflowix from '../components/WhyAiflowix';
+import Services from '../components/Services';
+import Industries from '../components/Industries';
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <WhyAiflowix />
+      <Services />
+      <Industries />
+    </main>
+  );
+};
+
+export default Home;
